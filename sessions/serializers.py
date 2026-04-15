ef23b8514c946f132f88a20cd190813026c7636c
sultanakona <sultanakona259@gmail.com>
2026-04-15 19:52:59 +0600

    Polished SessionSerializer to return absolute video URLs

--- sessions/serializers.py
+++ sessions/serializers.py
@@ -2,10 +2,21 @@ from rest_framework import serializers
 from .models import Session
 
 class SessionSerializer(serializers.ModelSerializer):
+    video_file = serializers.SerializerMethodField()
+
     class Meta:
         model = Session
         fields = ['id', 'user', 'total_kick', 'video_file', 'mode', 'is_story', 'is_shared_to_leaderboard', 'session_duration', 'countdown_time', 'created_at']
         read_only_fields = ['id', 'user', 'created_at']
+
+    def get_video_file(self, obj):
+        request = self.context.get('request')
+        if obj.video_file:
+            url = obj.video_file.url
+            if request is not None:
+                return request.build_absolute_uri(url)
+            return url
+        return None
 
 class SessionCompleteSerializer(serializers.Serializer):
     total_kick = serializers.IntegerField(required=True, min_value=0)
