038bc6d12293c741fbc9cbc4878d00ea101041ec
sultanakona <sultanakona259@gmail.com>
2026-04-15 19:07:25 +0600

    Fix missing imports in config/urls.py causing Render build failure

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.conf import settings
 from django.conf.urls.static import static
 
